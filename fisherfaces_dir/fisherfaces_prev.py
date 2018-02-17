c24f75081cce2257bd9cb7cd6ee8e26e02a67ea1
Shubhojit Mallick <shubhojitmallick@gmail.com>
2018-02-17 11:55:46 +0530

    commiting fisherfaces

--- fisherfaces_dir/fisherfaces_prev.py
+++ fisherfaces_dir/fisherfaces_prev.py
@@ -1,9 +1,6 @@
 
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
@@ -14,7 +11,6 @@ from skimage import img_as_float
 train_info = []
 
 
-# In[2]:
 
 
 ids = []
@@ -26,13 +22,7 @@ with open("train_images.csv") as f_train_images:
         i = i+1
 
 
-# In[3]:
-
-
 print train_info[0]
-
-
-# In[4]:
 
 
 #n_patches = len(train_info)
@@ -41,34 +31,19 @@ n = 32*32
 train_data = np.zeros([n_train_images, n])
 
 
-# In[5]:
-
-
 for i in range(n_train_images):
     img = cv2.imread('train_images/%s'%train_info[i][0])
     train_data[i,0:1024] = np.matrix(img_as_float(img[0:32,0:32,1]).flatten())
     
 
 
-# In[6]:
-
-
 print train_data.shape
-
-
-# In[7]:
 
 
 print train_data.mean(0)
 
 
-# In[8]:
-
-
 print train_data.mean(0).shape
-
-
-# In[9]:
 
 
 train_data_ms =np.matrix(train_data- np.transpose(train_data.mean(0)))
@@ -77,21 +52,11 @@ train_data_mean = np.matrix(np.mean(train_data[:,0:1024],axis=0))
 cov_matrix = (1.0/(n_train_images-1))*np.transpose(train_data_ms)*train_data_ms
 
 
-# In[10]:
-
-
 from numpy import linalg as la
 E,P = la.eig(cov_matrix)
 
-
-# In[11]:
-
-
 eig_pairs_pca = [(np.abs(E[i]), P[:,i]) for i in range(len(E))]
 eig_pairs_pca = sorted(eig_pairs_pca, key=lambda k: k[0], reverse=True)
-
-
-# In[12]:
 
 
 print('Variance:\n')
@@ -104,21 +69,14 @@ for i,j in enumerate(eig_pairs_pca):
         break
 
 
-# In[13]:
-
-
 print np.matrix(P[0:30])[0,:]
 #train_data_pca = train_data
 
-
-# In[14]:
 
 
 train_data_pca = train_data*np.transpose(np.matrix(P[0:k]))
 print train_data_pca.shape
 
-
-# In[15]:
 
 
 print E
@@ -129,14 +87,8 @@ plt.show()
 plt.savefig('Eigen vals.jpg')
 
 
-# In[16]:
-
-
 indices = [i for i,x in enumerate(ids[0:2000]) if x==1]
 print len(indices)
-
-
-# In[17]:
 
 
 np.set_printoptions(precision=4)
@@ -152,13 +104,7 @@ for cl in range(num_of_classes):
    # print('Mean Vector class %s: %s\n' %(cl, mean_vectors[cl-1]))
 
 
-# In[18]:
-
-
 #yt = train_data[[indices],:][0].shape
-
-
-# In[19]:
 
 
 S_W = np.zeros((k,k))
@@ -177,9 +123,6 @@ print('within-class Scatter Matrix:\n', S_W)
 #print class_samples_11.shape
 
 
-# In[20]:
-
-
 S_B = np.zeros((k,k))
 for cl in range(num_of_classes):
     N_cl = len([i for i,x in enumerate(ids[0:2000]) if x ==cl])
@@ -187,34 +130,19 @@ for cl in range(num_of_classes):
 print ('within-class Scatter Matrix:\n', S_B)
 
 
-# In[21]:
-
-
 print S_B.shape
 
 
-# In[22]:
-
-
 eig_vals, eig_vecs = np.linalg.eig(np.linalg.inv(S_W).dot(S_B))
-
-
-# In[23]:
 
 
 eig_pairs = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
 eig_pairs = sorted(eig_pairs, key=lambda k: k[0], reverse=True)
 
 
-# In[24]:
-
-
 print('Eigenvalues in decreasing order:\n')
 for i in eig_pairs:
     print(i[0])
-
-
-# In[25]:
 
 
 print('Variance explained:\n')
@@ -223,31 +151,21 @@ for i,j in enumerate(eig_pairs):
     print('eigenvalue {0:}: {1:.2%}'.format(i+1, (j[0]/eigv_sum).real))
 
 
-# In[26]:
-
-
 W = np.hstack((eig_pairs[0][1].reshape(k,1), eig_pairs[1][1].reshape(k,1),
                eig_pairs[2][1].reshape(k,1),eig_pairs[3][1].reshape(k,1),
                eig_pairs[4][1].reshape(k,1)))
 print('Matrix W:\n', W.real)
 
 
-# In[27]:
-
 
 yt=train_data_pca*W
 print yt
 
 
-# In[ ]:
-
 
 #Eigen db
 encoding_mat = np.transpose(np.matrix(P[0:k]))*W.real
 transformed_data = train_data_pca*W.real
-
-
-# In[ ]:
 
 
 ids_test = []
