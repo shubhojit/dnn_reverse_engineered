4d118a6a2253094bb8eefbb97e5b04d43e28dfa0
Shubhojit Mallick <shubhojitmallick@gmail.com>
2018-03-13 23:31:16 +0530

    Bug fixes in hidden dimensions

--- classify_text_cnn/cnn_recurrent.py
+++ classify_text_cnn/cnn_recurrent.py
@@ -5,41 +5,41 @@ import string
 
 from keras import backend
 from keras.layers import Dense, Input, Lambda, LSTM, TimeDistributed
-from keras.layers.merge import concatenate
-from keras.layers.embeddings import Embedding
+from keras.layers.embd import Embedding
 from keras.models import Model
+from keras.layers.merge import concatenate
 
 word2vec = gensim.models.Word2Vec.load("word2vec.gensim")
 
-# In order to represent unseen words and the null token; added an additional row of zeros to the embeddings matrix.
-embeddings = np.zeros((word2vec.syn0.shape[0] + 1, word2vec.syn0.shape[1]), dtype = "float32")
-embeddings[:word2vec.syn0.shape[0]] = word2vec.syn0
+# In order to represent unseen words & null token; added an additional row of zeros to the embedding matrix.
+embd = np.zeros((word2vec.syn0.shape[0] + 1, word2vec.syn0.shape[1]), dtype = "float32")
+embd[:word2vec.syn0.shape[0]] = word2vec.syn0
 
 maximum_tokens = word2vec.syn0.shape[0]
-embedding_dim = word2vec.syn0.shape[1]
-hidden_dim_1 = 200
-hidden_dim_2 = 100
+embedding_dimension = word2vec.syn0.shape[1]
+hidden_dimension_1 = 200
+hidden_dimension_2 = 100
 NUM_CLASSES = 10
 
 document = Input(shape = (None, ), dtype = "int32")
 left_context = Input(shape = (None, ), dtype = "int32")
 right_context = Input(shape = (None, ), dtype = "int32")
 
-embedder = Embedding(maximum_tokens + 1, embedding_dim, weights = [embeddings], trainable = False)
+embedder = Embedding(maximum_tokens + 1, embedding_dimension, weights = [embd], trainable = False)
 document_embedding = embedder(document)
 left_embedding = embedder(left_context)
 right_embedding = embedder(right_context)
 
 
-forward = LSTM(hidden_dim_1, return_sequences = True)(left_embedding) # Using LSTM RNNs instead of vanilla RNNs. As detailed in equation (1) of the paper.
-backward = LSTM(hidden_dim_1, return_sequences = True, go_backwards = True)(right_embedding) # equation (2) of the paper.
+forward = LSTM(hidden_dimension_1, return_sequences = True)(left_embedding) # Using LSTM RNNs instead of vanilla RNNs. As detailed in equation (1) of the paper.
+backward = LSTM(hidden_dimension_1, return_sequences = True, go_backwards = True)(right_embedding) # equation (2) of the paper.
 together = concatenate([forward, document_embedding, backward], axis = 2) # Equation (3).
 
-semantic = TimeDistributed(Dense(hidden_dim_2, activation = "tanh"))(together) # Equation (4) of the paper using tanh activation .
+semantic = TimeDistributed(Dense(hidden_dimension_2, activation = "tanh"))(together) # Equation (4) of the paper using tanh activation .
 
-pool_rnn = Lambda(lambda x: backend.max(x, axis = 1), output_shape = (hidden_dim_2, ))(semantic) # See equation (5).
+pool_rnn = Lambda(lambda x: backend.max(x, axis = 1), output_shape = (hidden_dimension_2, ))(semantic) # See equation (5).
 
-output = Dense(NUM_CLASSES, input_dim = hidden_dim_2, activation = "softmax")(pool_rnn) # Equations (6) &(7) of the paper.
+output = Dense(NUM_CLASSES, input_dimension = hidden_dimension_2, activation = "softmax")(pool_rnn) # Equations (6) &(7) of the paper.
 
 model = Model(inputs = [document, left_context, right_context], outputs = output)
 model.compile(optimizer = "adadelta", loss = "categorical_crossentropy", metrics = ["accuracy"])
